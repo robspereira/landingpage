5d2c01f33f4ab31c934bfe6feba1cd28346e5a02
robspereira <robertolucas335@hotmail.com>
2024-02-20 11:07:32 -0300

    Correção

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -5,7 +5,7 @@ export const Navigation = (props) => {
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
       <div className="container">
         <div className="navbar-header">
-          {/* <button
+          <button
             type="button"
             className="navbar-toggle collapsed"
             data-toggle="collapse"
@@ -16,7 +16,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
             <span className="icon-bar"></span>{" "}
             <span className="icon-bar"></span>{" "}
-          </button> */}
+          </button>
           <a className="navbar-brand page-scroll" href="#">
             <img className="logo" src="img/logo/Logo.png" alt="" width="50px;"/>
           </a>
